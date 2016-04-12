275132c988f5257d3c2845da9d2d1badc1034aef
FatemaT <f.alsuwairi@gmail.com>
2016-04-12 01:33:40 -0500

    navigationbar

--- TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/MainScreen.java
+++ TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/MainScreen.java
@@ -1,6 +1,7 @@
 package com.cs442.faaltene.touristassist;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,6 +23,8 @@ public class MainScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         submit = (Button) findViewById(R.id.submit);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "green avocado.ttf");
+        submit.setTypeface(tf);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
