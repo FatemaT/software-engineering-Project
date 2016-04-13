475d4e1d66442de2808426483256251ab02dac7b
FatemaT <f.alsuwairi@gmail.com>
2016-04-12 20:38:43 -0500

    Libray is up

--- TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/MainNavigation.java
+++ TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/MainNavigation.java
@@ -1,5 +1,7 @@
 package com.cs442.faaltene.touristassist;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v4.app.Fragment;
 import android.app.FragmentTransaction;
 import android.net.Uri;
@@ -16,6 +18,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import models.Hotel;
+import models.Mall;
+import models.Restaurant;
+import models.Showtime;
 
 public class MainNavigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -23,15 +31,24 @@ public class MainNavigation extends AppCompatActivity
         hospital_frag.OnFragmentInteractionListener, hotel_frag.OnFragmentInteractionListener,
         mall_frag.OnFragmentInteractionListener, museum_frag.OnFragmentInteractionListener,
         show_frag.OnFragmentInteractionListener{
-
+        TextView city;
+    Hotel[] hotels;
+    Showtime[] showtimes;
+    Restaurant[] restaurants;
+    Mall[] malls;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        Intent i= getIntent();
+        city = (TextView) findViewById(R.id.citytv);
+        String citys = i.getStringExtra("city");
+        hotels = i.getParcelableArrayExtra("hotels");
+        Typeface tf = Typeface.createFromAsset(getAssets(), "future.ttf");
+        city.setTypeface(tf);
+        city.setText("City of " + citys);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
