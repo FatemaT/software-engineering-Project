5c56a816c312edc31eb377dc6fcf0ad565bd69a8
FatemaT <f.alsuwairi@gmail.com>
2016-04-20 14:56:50 -0500

    showtime is up

---
+++ TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/Show_detail.java
@@ -0,0 +1,217 @@
+package com.cs442.faaltene.touristassist;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import java.util.ArrayList;
+
+import models.Mall;
+import models.Restaurant;
+import models.Review;
+import models.Showtime;
+
+public class Show_detail extends AppCompatActivity {
+    Showtime[] showtimes;
+    String TAG = "Response";
+    Review[] reviews;
+    TextView Sname;
+    TextView Sad;
+    TextView Sinfo;
+    ListView Srev;
+    TextView rating;
+    int Sid2;
+    Context mContext;
+    String Sid;
+    TextView review;
+    ArrayList<Review> rev;
+    ArrayAdapter<Review> revad;
+    TextView Sdur;
+    TextView Sshows;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        reviews = (Review[])intent.getSerializableExtra("reviews");
+        Sname = (TextView)findViewById(R.id.sname);
+        Sad = (TextView)findViewById(R.id.sad);
+        Sinfo = (TextView)findViewById(R.id.sinfo);
+        Sdur = (TextView)findViewById(R.id.sdur);
+        String name = intent.getStringExtra("Sname");
+        String add=intent.getStringExtra("Sad");
+        String info=intent.getStringExtra("Sinfo");
+        String dur=intent.getStringExtra("Sdur");
+        String coord=intent.getStringExtra("Scoord");
+        String shows=intent.getStringExtra("Sshows");
+        Sid = intent.getStringExtra("Sid");
+        Sid2 = Integer.parseInt(Sid);
+        rev = new ArrayList<Review>();
+        Sname = (TextView) findViewById(R.id.sname);
+        Sname.setText(name);
+        Sad = (TextView) findViewById(R.id.sad);
+        Sad.setText(add);
+        Sinfo = (TextView) findViewById(R.id.sinfo);
+        Sinfo.setText(info);
+        Sdur = (TextView) findViewById(R.id.sdur);
+        Sdur.setText(dur);
+        Srev = (ListView)findViewById(R.id.srev);
+        Sshows.setText(shows);
+        Sshows = (TextView) findViewById(R.id.sshows);
+
+
+
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "onPreExecute");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.i(TAG, "doInBackground");
+            //retrieveShowtimes();
+            //retrieveHotels();
+            //retrieveRestaurants();
+            //retrieveMalls();
+            //retrieveHospitals();
+            //retrieveClubs();
+            //retrieveAttractions();
+            retrieveReviews();
+            for(int i = 0; i<reviews.length; i++){
+                if(reviews[i].getEntityId().equalsIgnoreCase(Sid)){
+                    rev.add(reviews[i]);
+                }
+            }
+            //retrieveCity();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i(TAG, "onPostExecute");
+            //i.putExtra("city", city);
+            //i.putExtra("hotels", hotels);
+            //i.putExtra("hospitals",hospitals);
+            //i.putExtra("showtimes",showtimes);
+            //i.putExtra("restaurants", restaurants);
+            //i.putExtra("malls",malls);
+            //i.putExtra("clubs",clubs);
+            //i.putExtra("reviews",reviews);
+            //startActivity(i);
+            // Toast.makeText(MainActivity.this, "Response" + re, Toast.LENGTH_LONG).show();
+            mContext = getApplicationContext();
+            if (!rev.isEmpty()){
+                Srev.setAdapter(new ArrayAdapter<Review>(mContext, R.layout.list_item, rev) {
+
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View v = null;
+                        if (v == null) {
+                            if (Show_detail.this != null) {
+                                LayoutInflater vi = (LayoutInflater) Show_detail.this
+                                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                                v = vi.inflate(R.layout.list_item, null);
+                            }
+                        }
+
+                        rating = (TextView) v.findViewById(R.id.rating);
+                        review = (TextView) v.findViewById(R.id.review);
+
+                        rating.setText(rev.get(position).getRating());
+                        review.setText(rev.get(position).getReview());
+
+                        return v;
+                    }
+
+
+                });
+
+            }else{
+                Srev.setVisibility(View.GONE);
+            }
+        }
+
+    }
+    public static Review[] parseReviews(SoapObject soap)
+    {
+        Review[] reviews = new Review[soap.getPropertyCount()];
+        for (int i = 0; i < reviews.length; i++) {
+            SoapObject pii = (SoapObject)soap.getProperty(i);
+            Review review = new Review();
+
+            review.setEntityId(pii.getProperty(0).toString());
+            review.setRating(pii.getProperty(1).toString());
+            review.setReview(pii.getProperty(2).toString());
+            review.setReviewId(pii.getProperty(3).toString());
+            reviews[i] = review;
+
+        }
+        return reviews;
+    }
+    public void retrieveReviews() {
+        String SOAP_ACTION = "http://main.ta.se.cs.com/getReviews";
+        String METHOD_NAME = "getReviews";
+        String NAMESPACE = "http://main.ta.se.cs.com/";
+        String URL = "http://10.0.2.2:7101/SoftwareEngineeringHostServices-ViewController-context-root/TouristAssistServicePort?wsdl";
+
+        try {
+            SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+            Sid = Sid2+"";
+            Request.addProperty("arg0" ,Sid);
+
+            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            soapEnvelope.setOutputSoapObject(Request);
+
+            HttpTransportSE transport = new HttpTransportSE(URL);
+
+            transport.call(SOAP_ACTION, soapEnvelope);
+
+            SoapObject soapObject = (SoapObject) soapEnvelope.getResponse();
+            reviews = parseReviews(soapObject);
+
+            String re = soapObject.getProperty(0).toString();
+            // System.out.println("*************" +re);
+
+
+            Log.i(TAG, "Result : " + re);
+        } catch (Exception ex) {
+            Log.e(TAG, "Error: " + ex.getMessage());
+        }
+    }
+}
--- TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/show_frag.java
+++ TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/show_frag.java
@@ -1,17 +1,27 @@
 package com.cs442.faaltene.touristassist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 import java.util.ArrayList;
 
+import models.City;
 import models.Mall;
 import models.Showtime;
 
@@ -19,7 +29,7 @@ import models.Showtime;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link show_frag.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link show_frag#newInstance} factory method to
  * create an instance of this fragment.
@@ -30,7 +40,13 @@ public class show_frag extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     ListView showtime;
+    String cityid;
+    Context mContext;
     Showtime[] showtimes;
+    City city;
+    String cityname;
+    int cid;
+    String TAG = "Response";
     ArrayList<String> sho;
     ArrayAdapter<String> showad;
     View rootView;
@@ -69,6 +85,13 @@ public class show_frag extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        city = (City) getArguments().getSerializable("city");
+        cityid = city.getCityId();
+        cityname = city.getCityName();
+        Log.i(TAG, "Result : " + cityname);
+        cid = Integer.parseInt(cityid);
+        AsyncCallWS task = new AsyncCallWS();
+        task.execute();
     }
 
     @Override
@@ -80,12 +103,71 @@ public class show_frag extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_mall_frag, container, false);
         sho = new ArrayList<String>();
         showtime = (ListView)rootView.findViewById(R.id.mall_list);
-        for (int i = 0; i<showtimes.length; i++){
-            sho.add(showtimes[i].getShowtimeName());
-        }
-        showad = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, sho);
-        showtime.setAdapter(showad);
+
+
         return rootView;
+    }
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "onPreExecute");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.i(TAG, "doInBackground");
+            //retrieveShowtimes();
+            //retrieveHotels();
+            retrieveShowtimes();
+            for (int i = 0; i<showtimes.length; i++){
+                sho.add(showtimes[i].getShowtimeName());
+            }
+            //retrieveMalls();
+            //retrieveHospitals();
+            //retrieveClubs();
+            //retrieveAttractions();
+            //retrieveReviews();
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i(TAG, "onPostExecute");
+            //i.putExtra("city", city);
+            //i.putExtra("hotels", hotels);
+            //i.putExtra("hospitals",hospitals);
+            //i.putExtra("showtimes",showtimes);
+            //i.putExtra("restaurants", restaurants);
+            //i.putExtra("malls",malls);
+            //i.putExtra("clubs",clubs);
+            //i.putExtra("reviews",reviews);
+            //startActivity(i);
+            // Toast.makeText(MainActivity.this, "Response" + re, Toast.LENGTH_LONG).show();
+            showad = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, sho);
+            showtime.setAdapter(showad);
+            mContext = getActivity().getApplicationContext();
+            showtime.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent i = new Intent(mContext, Restaurant_detail.class);
+                    //i.putExtra("reviews",reviews);
+                    i.putExtra("showtime", showtimes[position]);
+                    i.putExtra("Sname", showtimes[position].getShowtimeName());
+                    i.putExtra("Sinfo", showtimes[position].getShowtimeDetails());
+                    i.putExtra("Sad", showtimes[position].getShowtimeAddress());
+                    i.putExtra("Sdur", showtimes[position].getShowtimeDuration());
+                    i.putExtra("Scoord", showtimes[position].getShowtimeCoordinates());
+                    i.putExtra("Sid", showtimes[position].getShowtimeId());
+                    i.putExtra("Sshows", showtimes[position].getShowtimeShows());
+                    startActivity(i);
+
+                }
+            });
+        }
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -112,6 +194,8 @@ public class show_frag extends Fragment {
         mListener = null;
     }
 
+
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -125,5 +209,57 @@ public class show_frag extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public void retrieveShowtimes() {
+        String SOAP_ACTION = "http://main.ta.se.cs.com/getShowtimes";
+        String METHOD_NAME = "getShowtimes";
+        String NAMESPACE = "http://main.ta.se.cs.com/";
+        String URL = "http://10.0.2.2:7101/SoftwareEngineeringHostServices-ViewController-context-root/TouristAssistServicePort?wsdl";
+
+        try {
+            SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+            cityid = cityid +"";
+            Request.addProperty("arg0" ,cityid);
+
+            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            soapEnvelope.setOutputSoapObject(Request);
+
+            HttpTransportSE transport = new HttpTransportSE(URL);
+
+            transport.call(SOAP_ACTION, soapEnvelope);
+
+            SoapObject soapObject = (SoapObject) soapEnvelope.getResponse();
+            showtimes = parseShowtimes(soapObject);
+
+            String re = showtimes[0].getShowtimeDuration();
+            //System.out.println("*************" +re);
+
+
+            Log.i(TAG, "Result : " + re);
+        } catch (Exception ex) {
+            Log.e(TAG, "Error: " + ex.getMessage());
+        }
+    }
+
+    public static Showtime[] parseShowtimes(SoapObject soap)
+    {
+        Showtime[] showtimes = new Showtime[soap.getPropertyCount()];
+        for (int i = 0; i < showtimes.length; i++) {
+            SoapObject pii = (SoapObject)soap.getProperty(i);
+            Showtime showtime = new Showtime();
+
+            showtime.setCityId(pii.getProperty(0).toString());
+            showtime.setShotimeCity(pii.getProperty(1).toString());
+            showtime.setShowtimeAddress(pii.getProperty(2).toString());
+            showtime.setShowtimeCoordinates(pii.getProperty(3).toString());
+            showtime.setShowtimeDetails(pii.getProperty(4).toString());
+            showtime.setShowtimeDuration(pii.getProperty(5).toString());
+            showtime.setShowtimeId(pii.getProperty(6).toString());
+            showtime.setShowtimeName(pii.getProperty(7).toString());
+            showtime.setShowtimeShows(pii.getProperty(8).toString());
+            showtimes[i] = showtime;
+        }
+        return showtimes;
     }
 }
