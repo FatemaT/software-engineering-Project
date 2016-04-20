d3ad4f06cba1cc9980ff9397e38e23632f597deb
FatemaT <f.alsuwairi@gmail.com>
2016-04-20 14:09:52 -0500

    mall fragment and detail is up

--- TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/Mall_detail.java
+++ TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/Mall_detail.java
@@ -2,11 +2,13 @@ package com.cs442.faaltene.touristassist;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +16,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 
 import java.util.ArrayList;
 
@@ -23,15 +30,19 @@ import models.Review;
 public class Mall_detail extends AppCompatActivity {
     Mall[] mall;
     Review[] reviews;
+    String TAG = "Response";
     TextView mname;
     TextView mad;
     TextView minfo;
     ListView mrev;
     TextView mb;
     TextView ms;
+    Context mContext;
     TextView rating;
     TextView review;
     ArrayList<Review> rev;
+    String mid;
+    int mid2;
     ArrayAdapter<Review> revad;
 
     @Override
@@ -54,7 +65,8 @@ public class Mall_detail extends AppCompatActivity {
         String brand=intent.getStringExtra("MBrand");
         String store=intent.getStringExtra("MBrand");
         String coord=intent.getStringExtra("MCoord");
-        String mid = intent.getStringExtra("Mid");
+        mid = intent.getStringExtra("Mid");
+        mid2 = Integer.parseInt(mid);
         rev = new ArrayList<Review>();
         mname = (TextView) findViewById(R.id.mname);
         mname.setText(name);
@@ -67,42 +79,128 @@ public class Mall_detail extends AppCompatActivity {
         ms = (TextView) findViewById(R.id.mS);
         ms.setText(store);
         mrev = (ListView)findViewById(R.id.mrev);
-        for(int i = 0; i<reviews.length; i++){
-            if(reviews[i].getEntityId().equalsIgnoreCase(mid)){
-                rev.add(reviews[i]);
-            }
-        }
-        if (!rev.isEmpty()){
-            mrev.setAdapter(new ArrayAdapter<Review>(this,
-                    R.layout.list_item, rev) {
 
-                @Override
-                public View getView(int position, View convertView, ViewGroup parent) {
-                    View v = null;
-                    if (v == null) {
-                        if (Mall_detail.this != null) {
-                            LayoutInflater vi = (LayoutInflater) Mall_detail.this
-                                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                            v = vi.inflate(R.layout.list_item, null);
+
+
+    }
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "onPreExecute");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.i(TAG, "doInBackground");
+            //retrieveShowtimes();
+            //retrieveHotels();
+            //retrieveRestaurants();
+            //retrieveMalls();
+            //retrieveHospitals();
+            //retrieveClubs();
+            //retrieveAttractions();
+            retrieveReviews();
+            for(int i = 0; i<reviews.length; i++){
+                if(reviews[i].getEntityId().equalsIgnoreCase(mid)){
+                    rev.add(reviews[i]);
+                }
+            }
+            //retrieveCity();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i(TAG, "onPostExecute");
+            //i.putExtra("city", city);
+            //i.putExtra("hotels", hotels);
+            //i.putExtra("hospitals",hospitals);
+            //i.putExtra("showtimes",showtimes);
+            //i.putExtra("restaurants", restaurants);
+            //i.putExtra("malls",malls);
+            //i.putExtra("clubs",clubs);
+            //i.putExtra("reviews",reviews);
+            //startActivity(i);
+            // Toast.makeText(MainActivity.this, "Response" + re, Toast.LENGTH_LONG).show();
+            mContext = getApplicationContext();
+            if (!rev.isEmpty()){
+                mrev.setAdapter(new ArrayAdapter<Review>(mContext, R.layout.list_item, rev) {
+
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View v = null;
+                        if (v == null) {
+                            if (Mall_detail.this != null) {
+                                LayoutInflater vi = (LayoutInflater) Mall_detail.this
+                                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                                v = vi.inflate(R.layout.list_item, null);
+                            }
                         }
+
+                        rating = (TextView) v.findViewById(R.id.rating);
+                        review = (TextView) v.findViewById(R.id.review);
+
+                        rating.setText(rev.get(position).getRating());
+                        review.setText(rev.get(position).getReview());
+
+                        return v;
                     }
 
-                    rating = (TextView) v.findViewById(R.id.rating);
-                    review = (TextView) v.findViewById(R.id.review);
 
-                    rating.setText(rev.get(position).getRating());
-                    review.setText(rev.get(position).getReview());
+                });
 
-                    return v;
-                }
-
-
-            });
-
-        }else{
-            mrev.setVisibility(View.GONE);
+            }else{
+                mrev.setVisibility(View.GONE);
+            }
         }
 
+    }
+    public static Review[] parseReviews(SoapObject soap)
+    {
+        Review[] reviews = new Review[soap.getPropertyCount()];
+        for (int i = 0; i < reviews.length; i++) {
+            SoapObject pii = (SoapObject)soap.getProperty(i);
+            Review review = new Review();
+
+            review.setEntityId(pii.getProperty(0).toString());
+            review.setRating(pii.getProperty(1).toString());
+            review.setReview(pii.getProperty(2).toString());
+            review.setReviewId(pii.getProperty(3).toString());
+            reviews[i] = review;
+
+        }
+        return reviews;
+    }
+    public void retrieveReviews() {
+        String SOAP_ACTION = "http://main.ta.se.cs.com/getReviews";
+        String METHOD_NAME = "getReviews";
+        String NAMESPACE = "http://main.ta.se.cs.com/";
+        String URL = "http://10.0.2.2:7101/SoftwareEngineeringHostServices-ViewController-context-root/TouristAssistServicePort?wsdl";
+
+        try {
+            SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+            mid = mid2+"";
+            Request.addProperty("arg0" ,mid);
+
+            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            soapEnvelope.setOutputSoapObject(Request);
+
+            HttpTransportSE transport = new HttpTransportSE(URL);
+
+            transport.call(SOAP_ACTION, soapEnvelope);
+
+            SoapObject soapObject = (SoapObject) soapEnvelope.getResponse();
+            reviews = parseReviews(soapObject);
+
+            String re = soapObject.getProperty(0).toString();
+            // System.out.println("*************" +re);
+
+
+            Log.i(TAG, "Result : " + re);
+        } catch (Exception ex) {
+            Log.e(TAG, "Error: " + ex.getMessage());
+        }
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/mall_frag.java
+++ TouristAssist/app/src/main/java/com/cs442/faaltene/touristassist/mall_frag.java
@@ -3,8 +3,10 @@ package com.cs442.faaltene.touristassist;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import models.City;
 import models.Mall;
 import models.Review;
 
@@ -33,9 +41,14 @@ public class mall_frag extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     ListView mall;
+    String TAG = "Response";
     Context mContext;
     Mall[] malls;
     Review[] reviews;
+    String cityid;
+    int cid;
+    String cityname;
+    City city;
     ArrayList<String> mal;
     ArrayAdapter<String> malad;
     View rootView;
@@ -82,38 +95,77 @@ public class mall_frag extends Fragment {
         // Inflate the layout for this fragment
         ((MainNavigation) getActivity()).setActionBarTitle("Shopping Malls");
         malls = (Mall[]) getArguments().getSerializable("malls");
-        reviews = (Review[])getArguments().getSerializable("reviews");
         rootView = inflater.inflate(R.layout.fragment_mall_frag, container, false);
         mal = new ArrayList<String>();
-        malls = (Mall[]) getArguments().getSerializable("malls");
         mall = (ListView)rootView.findViewById(R.id.mall_list);
-        for (int i = 0; i<malls.length; i++){
-            mal.add(malls[i].getMallName());
-        }
-        malad = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, mal);
-        mall.setAdapter(malad);
-        mContext = getActivity().getApplicationContext();
-        mall.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(mContext, Mall_detail.class);
-                i.putExtra("reviews", reviews);
-                i.putExtra("restaurant", malls[position]);
-                i.putExtra("Mname", malls[position].getMallName());
-                i.putExtra("Minfo", malls[position].getMallDetails());
-                i.putExtra("Mad", malls[position].getMallAddress());
-                i.putExtra("MBrand", malls[position].getMall_brands());
-                i.putExtra("MStore", malls[position].getMall_stores());
-                i.putExtra("MCoord", malls[position].getCoordinates());
-                i.putExtra("Mid", malls[position].getMallId());
-                startActivity(i);
 
-            }
-        });
         return rootView;
 
     }
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
 
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "onPreExecute");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.i(TAG, "doInBackground");
+            //retrieveShowtimes();
+            //retrieveHotels();
+            //retrieveRestaurants();
+            retrieveMalls();
+            for (int i = 0; i<malls.length; i++){
+                mal.add(malls[i].getMallName());
+            }
+
+
+            //retrieveHospitals();
+            //retrieveClubs();
+            //retrieveAttractions();
+            //retrieveReviews();
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i(TAG, "onPostExecute");
+            //i.putExtra("city", city);
+            //i.putExtra("hotels", hotels);
+            //i.putExtra("hospitals",hospitals);
+            //i.putExtra("showtimes",showtimes);
+            //i.putExtra("restaurants", restaurants);
+            //i.putExtra("malls",malls);
+            //i.putExtra("clubs",clubs);
+            //i.putExtra("reviews",reviews);
+            //startActivity(i);
+            // Toast.makeText(MainActivity.this, "Response" + re, Toast.LENGTH_LONG).show();
+            malad = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, mal);
+            mall.setAdapter(malad);
+            mContext = getActivity().getApplicationContext();
+            mall.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent i = new Intent(mContext, Mall_detail.class);
+                    i.putExtra("reviews", reviews);
+                    i.putExtra("restaurant", malls[position]);
+                    i.putExtra("Mname", malls[position].getMallName());
+                    i.putExtra("Minfo", malls[position].getMallDetails());
+                    i.putExtra("Mad", malls[position].getMallAddress());
+                    i.putExtra("MBrand", malls[position].getMall_brands());
+                    i.putExtra("MStore", malls[position].getMall_stores());
+                    i.putExtra("MCoord", malls[position].getCoordinates());
+                    i.putExtra("Mid", malls[position].getMallId());
+                    startActivity(i);
+
+                }
+            });
+        }
+
+    }
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
@@ -151,5 +203,53 @@ public class mall_frag extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+    public void retrieveMalls() {
+        String SOAP_ACTION = "http://main.ta.se.cs.com/getMalls";
+        String METHOD_NAME = "getMalls";
+        String NAMESPACE = "http://main.ta.se.cs.com/";
+        String URL = "http://10.0.2.2:7101/SoftwareEngineeringHostServices-ViewController-context-root/TouristAssistServicePort?wsdl";
+
+        try {
+            SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+            Request.addProperty("arg0" ,cityid);
+
+            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            soapEnvelope.setOutputSoapObject(Request);
+
+            HttpTransportSE transport = new HttpTransportSE(URL);
+
+            transport.call(SOAP_ACTION, soapEnvelope);
+
+            SoapObject soapObject = (SoapObject) soapEnvelope.getResponse();
+            malls = parseMalls(soapObject);
+
+            String re = malls[0].getMall_stores();
+            //System.out.println("*************" +re);
+
+
+            Log.i(TAG, "Result : " + re);
+        } catch (Exception ex) {
+            Log.e(TAG, "Error: " + ex.getMessage());
+        }
+    }
+    public static Mall[] parseMalls(SoapObject soap)
+    {
+        Mall[] malls = new Mall[soap.getPropertyCount()];
+        for (int i = 0; i < malls.length; i++) {
+            SoapObject pii = (SoapObject)soap.getProperty(i);
+            Mall mall = new Mall();
+            mall.setCity(pii.getProperty(0).toString());
+            mall.setCityId(pii.getProperty(1).toString());
+            mall.setCoordinates(pii.getProperty(2).toString());
+            mall.setMallAddress(pii.getProperty(3).toString());
+            mall.setMallDetails(pii.getProperty(4).toString());
+            mall.setMallId(pii.getProperty(5).toString());
+            mall.setMallName(pii.getProperty(6).toString());
+            mall.setMall_brands(pii.getProperty(7).toString());
+            mall.setMall_stores(pii.getProperty(8).toString());
+            malls[i] = mall;
+        }
+        return malls;
     }
 }
